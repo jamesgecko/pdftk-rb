af9e3a7d8dba2acf557a517e6c93890318d60daf
James D <jamesgecko@gmail.com>
2012-05-25 10:50:29 -0500

    Moving attribution to readme

--- pdftk.rb
+++ pdftk.rb
@@ -1,24 +1,9 @@
-# pdftk-ruby
-#
-# Port of pdftk - https://github.com/andrewheiss/pdftk-php/
-# License: Released under New BSD license
-#          http://www.opensource.org/licenses/bsd-license.php
-#
-# Authors: Andrew Heiss (www.andrewheiss.com)
-#          Sid Steward (http://www.oreillynet.com/pub/au/1754)
-
 class Pdftk
   def forge_fdf(pdf_form_url, fdf_data_strings, fdf_data_names, fields_hidden,
                 fields_readonly)
 
-    # forge_fdf, by Sid Steward
-    # version 1.1
-    # visit: www.pdfhacks.com/forge_fdf/
-    #
     # PDF can be particular about CR and LF characters, so I spelled them out 
     # in hex: CR == \x0d : LF == \x0a
-
-
     fdf = "%FDF-1.2\x0d%\xe2\xe3\xcf\xd3\x0d\x0a" # header
     fdf << "1 0 obj\x0d<< " # open the Root dictionary
     fdf << "\x0d/FDF << " # open the FDF dictionary
--- readme.md
+++ readme.md
@@ -1,6 +1,9 @@
 pdftk-rb
 ========
 
-This is a port of [pdftk-php](https://github.com/andrewheiss/pdftk-php) to Ruby.
+This is a port of [pdftk-php](https://github.com/andrewheiss/pdftk-php) 
+([Andrew Heiss](www.andrewheiss.com)) to Ruby.
+Based on [forge_fdf](http://www.pdfhacks.com/forge_fdf/) 1.1 by 
+[Sid Steward](http://www.oreillynet.com/pub/au/1754).
 
-Released under [New BSD license](http://www.opensource.org/licenses/bsd-license.php)
+Released under the [New BSD license](http://www.opensource.org/licenses/bsd-license.php)
