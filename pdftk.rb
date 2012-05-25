0f224e6639a11865d76ae8801099ae254b4f895d
James D <jamesgecko@gmail.com>
2012-05-25 15:18:26 -0500

    Fixing assorted errors. It should work now. :)

--- pdftk.rb
+++ pdftk.rb
@@ -18,7 +18,7 @@ class Pdftk
     fdf << "] \x0d" # close the Fields array
 
     # the PDF form filename or URL, if given
-    if pdf_form_url
+    if !pdf_form_url.empty?
       fdf << "/F (#{ escape_pdf_string($pdf_form_url) }) \x0d"
     end
 
@@ -39,7 +39,7 @@ class Pdftk
   def escape_pdf_string(pdf_string)
     result = ''
 
-    pdf_string.each do |char|
+    pdf_string.each_char do |char|
       case ord(char)
       when 0x28 || 0x29 || 0x5c # open paren, close paren, backslash
         backslash = 0x5c.chr
@@ -57,7 +57,7 @@ class Pdftk
   def escape_pdf_name(pdf_name)
     result = ''
 
-    pdf_name.each do |char|
+    pdf_name.each_char do |char|
       case ord(char)
       when 33...126 || 0x23 # hash mark
         result << char
@@ -74,14 +74,14 @@ class Pdftk
   # an array that uses dot-delimited names and returns a tree of arrays;
   #
   def burst_dots_into_arrays(fdf_data_old)
-    fdf_data_new = []
+    fdf_data_new = {}
 
     fdf_data_old.each do |key, value|
       key1, key2 = key.to_s.split('.', 2)
 
-      if key_split.count == 2 # handle dot
+      if !key2.nil? # handle dot
         if !fdf_data_new.include? key1
-          fdf_data_new[key1] = []
+          fdf_data_new[key1] = {}
         end
 
         if fdf_data_new[key1].class != Hash
@@ -118,8 +118,8 @@ class Pdftk
   def forge_fdf_fields_flags(fdf, field_name, fields_hidden, fields_readonly)
     set = "/SetFf"
     clear = "/ClrFf"
-    fdf << fields_hidden.includes(field_name) ? "#{set} 2 " : "#{clear} 2 "
-    fdf << fields_readonly.includes(field_name) ? "#{set} 1 " : "#{clear} 1 "
+    fdf << (fields_hidden.include?(field_name) ? "#{set} 2 " : "#{clear} 2 ")
+    fdf << (fields_readonly.include?(field_name) ? "#{set} 1 " : "#{clear} 1 ")
   end
 
   def forge_fdf_fields(fdf, fdf_data, fields_hidden, fields_readonly,
